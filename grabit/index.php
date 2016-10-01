e7418ffce05a9f0ecd65d4a6d7920897d3162589
DanHerePE <danielshim0911@gmail.com>
2016-09-30 20:26:27 -0700

    Delete index.php

--- grabit/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header(“Location:danherepe.github.io”);
-$file=“iplog.txt”; 
-$f=fopen($file,‘a’); 
-fwrite($f,“————————-“.”n”); 
-fwrite($f,“ipadr:”.$_SERVER[‘REMOTE_ADDR’].”n”);
-fclose($f); 
-?>
