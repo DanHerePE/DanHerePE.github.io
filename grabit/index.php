7ec03fc2a8cbdd66c3b71e0622f8c5b74556c135
DanHerePE <danielshim0911@gmail.com>
2016-09-30 20:19:18 -0700

    Update index.php

--- grabit/index.php
+++ grabit/index.php
@@ -1,11 +1,8 @@
 <?php
-header(“Location:index.html”);
+header(“Location:danherepe.github.io”);
 $file=“iplog.txt”; 
 $f=fopen($file,‘a’); 
 fwrite($f,“————————-“.”n”); 
-fwrite($f,“IP Address:”.$_SERVER[‘REMOTE_ADDR’].”n”); 
-fwrite($f,“User Agemt:”.$_SERVER[‘HTTP_USER_AGENT’].”n”); 
-fwrite($f,“Host Name:”.php_uname(‘n’).”n”); 
-fwrite($f,“Operating System:”.php_uname(‘v’).”(“.php_uname(‘s’).”)”.“n”); 
+fwrite($f,“ipadr:”.$_SERVER[‘REMOTE_ADDR’].”n”);
 fclose($f); 
 ?>
